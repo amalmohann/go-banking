f590bf5b68f4c8c27e2b3f8d0c282f4c1c749265
amalmohann <amalmohann@gmail.com>
2022-02-01 20:54:39 +0530

    Fixing the error

--- app/handlers.go
+++ app/handlers.go
@@ -2,9 +2,7 @@ package app
 
 import (
 	"encoding/json"
-	"encoding/xml"
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/amalmohann/banking/service"
@@ -23,27 +21,24 @@ func healthCheck(w http.ResponseWriter, r *http.Request) {
 
 // Find all customers handler
 func (ch *CustomerHandlers) getAllCustomers(w http.ResponseWriter, r *http.Request) {
-	customers, _ := ch.service.GetAllCustomers()
-	if r.Header.Get("Content-Type") == "application/json" {
+	customers, err := ch.service.GetAllCustomers()
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, err.Error())
+	} else {
 		w.Header().Add("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(customers)
-	} else {
-		w.Header().Add("Content-Type", "application/xml")
-		xml.NewEncoder(w).Encode(customers)
 	}
 }
 
 // find customer by id handler
 func (ch *CustomerHandlers) getCustomerById(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	p := r.URL.Query()
-	log.Print("back: ", p)
-	log.Print(params)
-	id := params["customer_id"]
-	customer, _ := ch.service.GetCustomerById(id)
-	if customer == nil {
+	p := mux.Vars(r)
+	id := p["customer_id"]
+	customer, err := ch.service.GetCustomerById(id)
+	if err != nil {
 		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "Customer Not Found")
+		fmt.Fprintf(w, err.Error())
 	} else {
 		w.Header().Add("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(customer)
